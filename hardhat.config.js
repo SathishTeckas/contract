dc72e8eeffece121932a0305bd6643976bb01a1e
Sathish M <153342012+SathishTeckas@users.noreply.github.com>
2025-03-18 19:05:16 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     sepolia: {
         url: "https://sepolia.infura.io/v3/579e486518ac4729b9b3a03bd413afbb",
-        accounts: ["29c596d9e57c0aa02e1385e10ed2e2ce21160d66e8041304e37ae5ff78f563be"],
+        accounts: ["29c596d9e57c0aa02e1385e10ed2e2ce21160dsdfsdf66e8041304e37ae5ff78f563be"],
     },
 }
 };
